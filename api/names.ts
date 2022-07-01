063f9c98618881b7b0ab8d9d0e88052b419bddc2
bitlatte <github@bitlatte.io>
2022-07-01 07:57:17 -0500

    Added Method Check

--- api/names.ts
+++ api/names.ts
@@ -8,17 +8,33 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 const handler = async (req: VercelRequest, res: VercelResponse) => {
-  const { data } = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: `Create a list of ${req.body.quantity} names from these seed words: ${req.body.seeds}`,
-    max_tokens: 60,
-    temperature: 0.8,
-    top_p: 1.0,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-  });
+  if (req.method === "POST") {
+    const { data } = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: `Create a list of ${req.body.quantity} names from these seed words: ${req.body.seeds}`,
+      max_tokens: 60,
+      temperature: 0.8,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
 
-  res.status(200).json({ names: data });
+    res.status(200).json({ names: data, success: true });
+  } else if (req.method === "GET") {
+    const { data } = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: `Create a list of 10 names from these seed words: pirate, cove, ship, fleet, oak, tavern, whiskey`,
+      max_tokens: 60,
+      temperature: 0.8,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+
+    res.status(200).json({ names: data, success: true });
+  } else {
+    res.status(500).json({ success: false });
+  }
 };
 
 export default handler;
